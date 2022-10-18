69a4c8ea108e3287d2da0cae21c790e439f66f8e
P-PeeP <nayeemhasnat00@gmail.com>
2022-10-18 08:51:39 +0600

    class name change

---
+++ src/main/java/string_manipulation/ConvertStringData.java
@@ -0,0 +1,26 @@
+/*
+4. Writea program that will give following output:
+        Input: chattogram
+        Output: C8M
+*/
+
+package string_manipulation;
+
+public class ConvertStringData {
+
+    public static void main(String[] args) {
+
+        String division="chattogram";
+        int index;
+
+        String firstIndex = String.valueOf(division.charAt(0)).toUpperCase();
+        String secondIndex = String.valueOf(division.charAt(9)).toUpperCase();
+
+        index = division.lastIndexOf('a');
+
+        String result = firstIndex + index + secondIndex;
+
+        System.out.println("Expected Output for chattogram: " +result);
+
+    }
+}
